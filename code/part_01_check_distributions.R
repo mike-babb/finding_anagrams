632a56217d0cf682943a9e9e8d41c31dae4bce70
Michael Babb <34557750+mike-babb@users.noreply.github.com>
2023-07-25 11:33:35 -0700

    exmination the distribution of number sums

---
+++ code/part_01_check_distributions.R
@@ -0,0 +1,17 @@
+# mike babb
+# 2023 06 10
+# process state to state data
+
+##
+# clean up
+##
+rm(list = ls())
+gc()
+
+library(data.table)
+library(ggplot2)
+library(magrittr)
+library(scales)
+library(cowplot)
+library(stringi)
+# library(xlsx)
